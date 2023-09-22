d3b35d5b8579d9dbcec915d6eb070845c31b7f3c
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2023-09-22 11:13:40 -0400

    research question addition

--- CLI.py
+++ CLI.py
@@ -6,6 +6,7 @@ import sys
 def cli_helper():
     parser = argparse.ArgumentParser(description='The CLI usage to get all your questions and answers')
     parser.add_argument('-ql', '--quizlet', nargs=1, help='Enter the quizlet link')
+    parser.add_argument('-q', '--getQuestion', help='Research a question inside the database')
     return parser
 
 if __name__ == '__main__':
@@ -18,5 +19,8 @@ if __name__ == '__main__':
         quizlet = quizletModule.quizlet_talker(quizlet_website=quizlet_link)
         page_data = quizlet.quizlet_communication_question_answer()
         dataHandle.add_dictionary_to_json(page_data)
-
+    if args.getQuestion:
+        answer = dataHandle.get_question_from_prompt(args.getQuestion)
+        for e in answer:
+            print(e)
 
